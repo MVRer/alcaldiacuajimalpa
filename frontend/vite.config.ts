fef6fe4acdbb84b62428b330a88d10795f879593
MVRer <mariano@deltadinamica.com>
2025-10-21 12:24:14 -0600

    SSL CAP

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     host: true,
+    https: fs.existsSync("/etc/ssl/private/selfsigned.key") ? {
+      key: fs.readFileSync("/etc/ssl/private/selfsigned.key"),
+      cert: fs.readFileSync("/etc/ssl/certs/selfsigned.crt"),
+    } : undefined,
+  },
+  preview: {
+    host: true,
+    https: fs.existsSync("/etc/ssl/private/selfsigned.key") ? {
+      key: fs.readFileSync("/etc/ssl/private/selfsigned.key"),
+      cert: fs.readFileSync("/etc/ssl/certs/selfsigned.crt"),
+    } : undefined,
   },
   build: {
     sourcemap: mode === "development",
